26bd60f6917c2022662e91698ec4164ef18f7c2c
akhil josef <ajosef@opentext.com>
2025-10-02 11:57:35 +0530

    Add project files.

---
+++ ElevatorApp/Elevator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorApp
+{
+    public class Elevator
+    {
+        public int ElevatorId { get; set; }
+        public int CurrentFloor { get; set; }
+        public bool IsMoving { get; set; }
+    }
+}
