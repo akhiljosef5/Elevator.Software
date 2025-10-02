fb62a31abbeadb5ff69e8391cdf10cab55c8d08d
akhil josef <ajosef@opentext.com>
2025-10-02 13:19:22 +0530

    initial simple approach

--- ElevatorApp/Elevator.cs
+++ ElevatorApp/Elevator.cs
@@ -10,6 +10,45 @@ namespace ElevatorApp
     {
         public int ElevatorId { get; set; }
         public int CurrentFloor { get; set; }
-        public bool IsMoving { get; set; }
+        public bool IsMoving { get; set; } = false;
+        public string Direction { get; set; } = "idle"; // "Up", "Down", or "Idle"
+
+        public static void MoveElevator(List<Elevator> elevators, int targetFloor)
+        {
+            foreach (Elevator e in elevators)
+            {
+                if(e.IsMoving)
+                {
+                    Console.WriteLine($"LOG INFO: elevator {e.ElevatorId} is busy");
+                }
+                else
+                {
+                    if(targetFloor == e.CurrentFloor)
+                    {
+                        e.IsMoving = false;
+                        e.Direction = "Idle";
+                        return;
+                    }
+                    e.IsMoving = true;
+                    if(targetFloor > e.CurrentFloor)
+                    {
+                        e.Direction = "Up";
+                        while(e.CurrentFloor < targetFloor)
+                        {
+                            e.CurrentFloor++;
+                        }
+                        return;
+                    }
+                    else
+                    {
+                        e.Direction = "Down";
+                        while(e.CurrentFloor > targetFloor)
+                        {
+                            e.CurrentFloor--;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
